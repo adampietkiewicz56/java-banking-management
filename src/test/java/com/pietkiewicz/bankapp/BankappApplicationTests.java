5eb7fa982d4706c138acc16f94d1b54baa888c70
Adam Pietkiewicz <a.pietkiewicz.999@studms.ug.edu.pl>
2026-04-28 16:56:24 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/pietkiewicz/bankapp/BankappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pietkiewicz.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
