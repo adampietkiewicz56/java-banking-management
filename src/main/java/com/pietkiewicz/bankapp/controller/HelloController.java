5b6f8a10095acea1934b98d7b7263731d22b86cb
Adam Pietkiewicz <a.pietkiewicz.999@studms.ug.edu.pl>
2026-04-28 17:09:16 +0200

    Add hello endpoint and basic security config

---
+++ src/main/java/com/pietkiewicz/bankapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pietkiewicz.bankapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Bank API works!";
+    }
+}
