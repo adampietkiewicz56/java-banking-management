1aec7336b69e34daded82f0f131045574d944d3a
Adam Pietkiewicz <a.pietkiewicz.999@studms.ug.edu.pl>
2026-04-29 11:52:53 +0200

    Secure API with JWT authorization

--- src/main/java/com/pietkiewicz/bankapp/config/SecurityConfig.java
+++ src/main/java/com/pietkiewicz/bankapp/config/SecurityConfig.java
@@ -5,17 +5,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/users", "/users/login").permitAll()
+                        .requestMatchers("/users/**").authenticated()
+                        .requestMatchers("/accounts/**").authenticated()
+                        .requestMatchers("/transactions/**").authenticated()
+                        .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
